831a1acf03ca0b400be6b121b54b86f752d4ee26
naddisaikumar <saikumarnaddi123@gmail.com>
2024-07-28 17:13:50 +0000

    react-codingpractice23

---
+++ src/components/BlogItemDetails/index.js
@@ -0,0 +1,77 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import './index.css'
+
+class BlogItemDetails extends Component {
+  state = {blogData: {}, isLoading: true, error: false}
+
+  componentDidMount() {
+    this.getBlogItemData()
+  }
+
+  getBlogItemData = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+
+    try {
+      const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+      if (!response.ok) {
+        throw new Error('Failed to fetch data')
+      }
+      const data = await response.json()
+      const updatedData = {
+        title: data.title,
+        imageUrl: data.image_url,
+        content: data.content,
+        avatarUrl: data.avatar_url,
+        author: data.author,
+      }
+
+      this.setState({blogData: updatedData, isLoading: false})
+    } catch (error) {
+      this.setState({error: true, isLoading: false})
+    }
+  }
+
+  renderBlogItemDetails = () => {
+    const {blogData} = this.state
+    const {title, imageUrl, content, avatarUrl, author} = blogData
+
+    return (
+      <div className="blog-info">
+        <h1 className="blog-details-title">{title}</h1>
+
+        <div className="author-details">
+          <img className="author-pic" src={avatarUrl} alt={author} />
+          <p className="details-author-name">{author}</p>
+        </div>
+
+        <img className="blog-image" src={imageUrl} alt={title} />
+        <p className="blog-content">{content}</p>
+      </div>
+    )
+  }
+
+  render() {
+    const {isLoading, error} = this.state
+
+    return (
+      <div className="blog-container">
+        {isLoading ? (
+          <div testid="loader">
+            <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
+          </div>
+        ) : error ? (
+          <p>Something went wrong. Please try again later.</p>
+        ) : (
+          this.renderBlogItemDetails()
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogItemDetails
